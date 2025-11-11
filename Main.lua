0ffb8748918df9ec422a5da99c0cccad706b9cc2
MarkTukalak <mark@pragmaflow.com>
2025-11-11 14:20:32 -0500

    made title and window

---
+++ Main.lua
@@ -0,0 +1,13 @@
+function love.load()
+    love.window.setTitle("Escapist")
+    love.window.setMode(800, 600)
+end
+function love.update(dt)
+
+end
+function love.draw()
+
+end
+function love.mousepressed(x, y, button)
+
+end
