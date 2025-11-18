df963225188a634f6fd76abd67d3d72d78a599ac
johnny958 <johnny@pragmaflow.com>
2025-11-18 13:14:31 -0500

    started menu scree

--- Main.lua
+++ Main.lua
@@ -1,12 +1,20 @@
 function love.load()
     love.window.setTitle("Escapist")
     love.window.setMode(800, 600)
+    require("menu")
+    initializeMenu ()
+    game_state = {
+        screen = "menu",
+        current_room = "room1"
+    }
 end
 function love.update(dt)
 
 end
 function love.draw()
-
+if game_state.screen == "menu" then
+        drawMenu()
+    end
 end
 function love.mousepressed(x, y, button)
 
---
+++ menu.lua
@@ -0,0 +1,20 @@
+function initializeMenu()
+    local startbtn = {
+        x = 300,
+        y = 400,
+        width = 200,
+        height = 50,
+        backgroundColor = {0.2, 0.6, 0.2},
+        haoverColor = {0.3, 0.7, 0.3},
+        onclick = start,
+        text = "Start Game"
+    }
+end
+
+function drawMenu()
+     
+end
+
+function start()
+    
+end
